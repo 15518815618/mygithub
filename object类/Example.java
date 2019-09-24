6ad965fc1f551532700791beb903d963eb8a45e6
15518815618 <55038555+15518815618@users.noreply.github.com>
2019-09-24 22:08:26 +0800

    Add files via upload

---
+++ object类/Example.java
@@ -0,0 +1,16 @@
+class Animal
+{
+	/*public  Animal(){
+	System.out.println(" ¶¯Îï½Ð !");
+	}*/
+	
+	public String toString(){
+	return "i am an animal !";}
+}
+public class Example
+{
+	public static void main(String[] args){
+	Animal animal=new Animal();
+	//System.out.println(animal.toString());
+	}
+}
